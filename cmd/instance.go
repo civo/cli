d24951afb315e56ec6450a1743d2a8fc17011ca5
Andy Jeffries <andy@andyjeffries.co.uk>
2020-03-10 14:25:36 +0000

    Add console and reboot commands

--- cmd/instance.go
+++ cmd/instance.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 	"strings"
 	"time"
 
@@ -55,13 +56,13 @@ Example: civo instance ls -o custom -f "ID: Name (PublicIP)"`,
 		client, err := config.CivoAPIClient()
 		if err != nil {
 			fmt.Printf("Unable to create a Civo API Client: %s\n", aurora.Red(err))
-			return
+			os.Exit(1)
 		}
 
 		instances, err := client.ListAllInstances()
 		if err != nil {
 			fmt.Printf("Unable to list instances: %s\n", aurora.Red(err))
-			return
+			os.Exit(1)
 		}
 
 		ow := utility.NewOutputWriter()
@@ -148,13 +149,13 @@ Example: civo instance show ID/NAME -o custom -f "Key1: Key2"`,
 		client, err := config.CivoAPIClient()
 		if err != nil {
 			fmt.Printf("Unable to create a Civo API Client: %s\n", aurora.Red(err))
-			return
+			os.Exit(1)
 		}
 
 		instance, err := client.FindInstance(args[0])
 		if err != nil {
 			fmt.Printf("Unable to search instances: %s\n", aurora.Red(err))
-			return
+			os.Exit(1)
 		}
 
 		ow := utility.NewOutputWriter()
@@ -222,7 +223,6 @@ Example: civo instance show ID/NAME -o custom -f "Key1: Key2"`,
 // TODO: instance tags ID/HOSTNAME 'tag1 tag2 tag3...' -- retag instance by ID (input no tags to clear all tags)
 // TODO: instance update ID/HOSTNAME [--name] [--notes] -- update details of instance
 
-// instanceRemoveCmd represents the command to show the details for an instance
 var instanceRemoveCmd = &cobra.Command{
 	Use:     "remove",
 	Aliases: []string{"delete", "destroy", "rm"},
@@ -234,30 +234,168 @@ Example: civo instance remove ID/NAME`,
 		client, err := config.CivoAPIClient()
 		if err != nil {
 			fmt.Printf("Unable to create a Civo API Client: %s\n", aurora.Red(err))
-			return
+			os.Exit(1)
 		}
 
 		instance, err := client.FindInstance(args[0])
 		if err != nil {
 			fmt.Printf("Finding instance: %s\n", aurora.Red(err))
-			return
+			os.Exit(1)
 		}
 
 		_, err = client.DeleteInstance(instance.ID)
 		if err != nil {
 			fmt.Printf("Removing instance: %s\n", aurora.Red(err))
-			return
+			os.Exit(1)
 		}
 
 		if OutputFormat == "human" {
 			fmt.Printf("The instance %s (%s) has been removed\n", aurora.Green(instance.Hostname), instance.ID)
+		} else {
+			ow := utility.NewOutputWriter()
+			ow.StartLine()
+			ow.AppendData("ID", instance.ID)
+			ow.AppendDataWithLabel("OpenstackServerID", instance.OpenstackServerID, "Openstack Server ID")
+			ow.AppendData("Hostname", instance.Hostname)
+			if OutputFormat == "json" {
+				ow.WriteSingleObjectJSON()
+			} else {
+				ow.WriteCustomOutput(OutputFields)
+			}
 		}
 	},
 }
 
-// TODO: instance reboot ID/HOSTNAME -- reboots instance with ID/hostname entered [hard-reboot]
+var instanceRebootCmd = &cobra.Command{
+	Use:     "reboot",
+	Aliases: []string{"hard-reboot"},
+	Short:   "Hard reboot an instance",
+	Long: `Pull the power and restart the specified instance by part of the ID or name.
+
+Example: civo instance reboot ID/NAME`,
+	Run: func(cmd *cobra.Command, args []string) {
+		client, err := config.CivoAPIClient()
+		if err != nil {
+			fmt.Printf("Unable to create a Civo API Client: %s\n", aurora.Red(err))
+			os.Exit(1)
+		}
+
+		instance, err := client.FindInstance(args[0])
+		if err != nil {
+			fmt.Printf("Finding instance: %s\n", aurora.Red(err))
+			os.Exit(1)
+		}
+
+		_, err = client.RebootInstance(instance.ID)
+		if err != nil {
+			fmt.Printf("Rebooting instance: %s\n", aurora.Red(err))
+			os.Exit(1)
+		}
+
+		if OutputFormat == "human" {
+			fmt.Printf("The instance %s (%s) is being rebooted\n", aurora.Green(instance.Hostname), instance.ID)
+		} else {
+			ow := utility.NewOutputWriter()
+			ow.StartLine()
+			ow.AppendData("ID", instance.ID)
+			ow.AppendDataWithLabel("OpenstackServerID", instance.OpenstackServerID, "Openstack Server ID")
+			ow.AppendData("Hostname", instance.Hostname)
+			if OutputFormat == "json" {
+				ow.WriteSingleObjectJSON()
+			} else {
+				ow.WriteCustomOutput(OutputFields)
+			}
+		}
+	},
+}
+
 // TODO: instance soft-reboot ID/HOSTNAME -- soft-reboots instance with ID entered
+var instanceSoftRebootCmd = &cobra.Command{
+	Use:   "soft-reboot",
+	Short: "Hard reboot an instance",
+	Long: `Pull the power and restart the specified instance by part of the ID or name.
+
+Example: civo instance reboot ID/NAME`,
+	Run: func(cmd *cobra.Command, args []string) {
+		client, err := config.CivoAPIClient()
+		if err != nil {
+			fmt.Printf("Unable to create a Civo API Client: %s\n", aurora.Red(err))
+			os.Exit(1)
+		}
+
+		instance, err := client.FindInstance(args[0])
+		if err != nil {
+			fmt.Printf("Finding instance: %s\n", aurora.Red(err))
+			os.Exit(1)
+		}
+
+		_, err = client.SoftRebootInstance(instance.ID)
+		if err != nil {
+			fmt.Printf("Soft-rebooting instance: %s\n", aurora.Red(err))
+			os.Exit(1)
+		}
+
+		if OutputFormat == "human" {
+			fmt.Printf("The instance %s (%s) is being soft-rebooted\n", aurora.Green(instance.Hostname), instance.ID)
+		} else {
+			ow := utility.NewOutputWriter()
+			ow.StartLine()
+			ow.AppendData("ID", instance.ID)
+			ow.AppendDataWithLabel("OpenstackServerID", instance.OpenstackServerID, "Openstack Server ID")
+			ow.AppendData("Hostname", instance.Hostname)
+			if OutputFormat == "json" {
+				ow.WriteSingleObjectJSON()
+			} else {
+				ow.WriteCustomOutput(OutputFields)
+			}
+		}
+	},
+}
+
 // TODO: instance console ID/HOSTNAME -- outputs a URL for a web-based console for instance with ID provided
+var instanceConsoleCmd = &cobra.Command{
+	Use:   "console",
+	Short: "Get console URL for instance",
+	Long: `Get the web console's URL for a given instance by part of the ID or name.
+
+Example: civo instance console ID/NAME`,
+	Run: func(cmd *cobra.Command, args []string) {
+		client, err := config.CivoAPIClient()
+		if err != nil {
+			fmt.Printf("Unable to create a Civo API Client: %s\n", aurora.Red(err))
+			os.Exit(1)
+		}
+
+		instance, err := client.FindInstance(args[0])
+		if err != nil {
+			fmt.Printf("Finding instance: %s\n", aurora.Red(err))
+			os.Exit(1)
+		}
+
+		url, err := client.GetInstanceConsoleURL(instance.ID)
+		if err != nil {
+			fmt.Printf("Getting console URL: %s\n", aurora.Red(err))
+			os.Exit(1)
+		}
+
+		if OutputFormat == "human" {
+			fmt.Printf("The instance %s (%s) has a console at %s\n", aurora.Green(instance.Hostname), instance.ID,
+				aurora.Green(url))
+		} else {
+			ow := utility.NewOutputWriter()
+			ow.StartLine()
+			ow.AppendData("ID", instance.ID)
+			ow.AppendDataWithLabel("URL", url, "Console URL")
+			ow.AppendData("Hostname", instance.Hostname)
+			if OutputFormat == "json" {
+				ow.WriteSingleObjectJSON()
+			} else {
+				ow.WriteCustomOutput(OutputFields)
+			}
+		}
+	},
+}
+
 // TODO: instance stop ID/HOSTNAME -- shuts down the instance with ID provided
 // TODO: instance start ID/HOSTNAME -- starts a stopped instance with ID provided
 // TODO: instance upgrade ID/HOSTNAME new-size -- Upgrade instance with ID to size provided (see civo sizes for size names)
@@ -271,4 +409,7 @@ func init() {
 	instanceCmd.AddCommand(instanceListCmd)
 	instanceCmd.AddCommand(instanceShowCmd)
 	instanceCmd.AddCommand(instanceRemoveCmd)
+	instanceCmd.AddCommand(instanceRebootCmd)
+	instanceCmd.AddCommand(instanceSoftRebootCmd)
+	instanceCmd.AddCommand(instanceConsoleCmd)
 }
