410abb5193bd01dfa92fcf921e4c4a0f2a72473b
Alejandro JNM <alejandrojnm@gmail.com>
2020-05-06 02:10:53 -0400

    feat(loadbalancer): Added the cmd loadbalancer
    Added the possibility the you can list, create, update and delete loadbalancer
    
    BREAKING CHANGE: No
    
    Signed-off-by: Alejandro JNM <alejandrojnm@gmail.com>

---
+++ cmd/loadbalancer_list.go
@@ -0,0 +1,91 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/civo/cli/config"
+	"github.com/civo/cli/utility"
+	"github.com/logrusorgru/aurora"
+	"github.com/spf13/cobra"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var loadBalancerListCmd = &cobra.Command{
+	Use:     "ls",
+	Aliases: []string{"list", "all"},
+	Short:   "List Load Balancer",
+	Long: `List all current Load Balancer.
+If you wish to use a custom format, the available fields are:
+
+	* ID
+	* Name
+	* Protocol
+	* Port
+	* TLSCertificate
+	* TLSKey
+	* Policy
+	* HealthCheckPath
+	* FailTimeout
+	* MaxConns
+	* IgnoreInvalidBackendTLS
+	* Backends
+
+Example: civo loadbalancer ls -o custom -f "ID: Name"`,
+	Run: func(cmd *cobra.Command, args []string) {
+		client, err := config.CivoAPIClient()
+		if err != nil {
+			fmt.Printf("Unable to create a Civo API Client: %s\n", aurora.Red(err))
+			os.Exit(1)
+		}
+
+		lbs, err := client.ListLoadBalancers()
+		if err != nil {
+			fmt.Printf("Unable to list Load Balancer: %s\n", aurora.Red(err))
+			os.Exit(1)
+		}
+
+		ow := utility.NewOutputWriter()
+		for _, lb := range lbs {
+			ow.StartLine()
+
+			ow.AppendData("ID", lb.ID)
+			ow.AppendData("Name", lb.Hostname)
+			ow.AppendData("Protocol", lb.Protocol)
+			ow.AppendData("Port", strconv.Itoa(lb.Port))
+
+			if outputFormat == "json" || outputFormat == "custom" {
+				ow.AppendDataWithLabel("TLSCertificate", lb.TLSCertificate, "TLS Cert")
+				ow.AppendDataWithLabel("TLSKey", lb.TLSKey, "TLS Key")
+				ow.AppendData("Policy", lb.Policy)
+				ow.AppendDataWithLabel("HealthCheckPath", lb.HealthCheckPath, "Health Check Path")
+				ow.AppendDataWithLabel("FailTimeout", strconv.Itoa(lb.FailTimeout), "Fail Timeout")
+				ow.AppendDataWithLabel("MaxConns", strconv.Itoa(lb.MaxConns), "Max. Connections")
+				ow.AppendDataWithLabel("IgnoreInvalidBackendTLS", strconv.FormatBool(lb.IgnoreInvalidBackendTLS), "Ignore Invalid Backend TLS?")
+			}
+
+			var backendList []string
+
+			for _, backend := range lb.Backends {
+				instance, err := client.FindInstance(backend.InstanceID)
+				if err != nil {
+					fmt.Printf("Unable to find the instance: %s\n", aurora.Red(err))
+					os.Exit(1)
+				}
+				backendList = append(backendList, instance.Hostname)
+			}
+
+			ow.AppendData("Backends", strings.Join(backendList, ", "))
+
+		}
+
+		switch outputFormat {
+		case "json":
+			ow.WriteMultipleObjectsJSON()
+		case "custom":
+			ow.WriteCustomOutput(outputFields)
+		default:
+			ow.WriteTable()
+		}
+	},
+}
--- go.mod
+++ go.mod
@@ -16,6 +16,7 @@ require (
 	github.com/spf13/cobra v0.0.5
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.3.2
 	golang.org/x/sys v0.0.0-20200122134326-e047566fdf82 // indirect
 	golang.org/x/text v0.3.2 // indirect
 	golang.org/x/tools v0.0.0-20191126055441-b0650ceb63d9 // indirect
