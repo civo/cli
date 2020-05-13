75fe68e8939d7234ad729e589b5a01dc95a8ac6d
Alejandro JNM <alejandrojnm@gmail.com>
2020-05-13 10:36:48 -0400

    fix(Version): update the version cmd
    - improved way of displaying data
    - added the verbose flag
    
    BREAKING CHANGE: No
    
    Signed-off-by: Alejandro JNM <alejandrojnm@gmail.com>

--- cmd/version.go
+++ cmd/version.go
@@ -10,7 +10,8 @@ import (
 )
 
 var (
-	quiet      = false
+	quiet      bool
+	verbose    bool
 	VersionCli = "dev"
 	CommitCli  = "none"
 	DateCli    = "unknown"
@@ -26,7 +27,8 @@ var (
 			}
 			res, _ := latest.Check(githubTag, strings.Replace(VersionCli, "v", "", 1))
 
-			if !quiet {
+			switch {
+			case verbose:
 				fmt.Printf("Client version: %s\n", VersionCli)
 				fmt.Printf("Go version (client): %s\n", runtime.Version())
 				fmt.Printf("Git commit (client): %s\n", CommitCli)
@@ -35,16 +37,20 @@ var (
 				if res.Outdated {
 					utility.YellowConfirm("A newer version (v%s) is available, please upgrade\n", res.Current)
 				}
-
-			} else {
+			case quiet:
 				fmt.Printf("%s\n", VersionCli)
+			default:
+				fmt.Printf("Civo CLI: %s\n", VersionCli)
+				if res.Outdated {
+					utility.YellowConfirm("A newer version (v%s) is available, please upgrade\n", res.Current)
+				}
 			}
-
 		},
 	}
 )
 
 func init() {
 	rootCmd.AddCommand(versionCmd)
-	versionCmd.Flags().BoolVarP(&quiet, "quiet", "q", false, "Use quiet output for version information.")
+	versionCmd.Flags().BoolVarP(&quiet, "quiet", "q", false, "Use quiet output for simple output.")
+	versionCmd.Flags().BoolVarP(&verbose, "verbose", "v", false, "Use verbose output to see full information.")
 }
