eb21b60bdfba09b296ea1cf40933503dcd76e5f0
Haardik Dharma <haardik@civo.com>
2022-02-15 21:20:17 +0530

    Update kubernetes_test.go

--- cmd/kubernetes_test.go
+++ cmd/kubernetes_test.go
@@ -27,6 +27,11 @@ func TestInstallApplications(t *testing.T) {
 			args: InstallApplicationsArgs{defaultApps: []string{"metrics-server", "traefik"}, apps: "foo", removeApps: ""},
 			want: []string{"metrics-server", "traefik", "foo"},
 		},
+		{
+			name: "Test Removal of Default Apps and Installation of other Apps",
+			args: InstallApplicationsArgs{defaultApps: []string{"metrics-server", "traefik"}, apps: "foo", removeApps: "traefik"},
+			want: []string{"metrics-server", "foo"},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
