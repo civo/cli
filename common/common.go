ce7910b9e3ea38399738649ce5bb6bcc3b0ffc42
Harsh Thakur <harsh@civo.com>
2022-06-29 18:35:32 +0530

    Make common package to store global root options
    Signed-off-by: Harsh Thakur <harsh@civo.com>

---
+++ common/common.go
@@ -0,0 +1,14 @@
+package common
+
+var (
+	// OutputFields for custom format output
+	OutputFields string
+	// OutputFormat for custom format output
+	OutputFormat string
+	// RegionSet picks the region to connect to, if you use this option it will use it over the default region
+	RegionSet string
+	// DefaultYes : automatic yes to prompts; assume \"yes\" as answer to all prompts and run non-interactively
+	DefaultYes bool
+	// PrettySet : Prints the json output in pretty format
+	PrettySet bool
+)
