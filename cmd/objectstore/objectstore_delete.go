720f5b8381e09be06b70d5c50c13ca9f53ea3a18
Haardik Dharma <haardik@civo.com>
2022-09-13 17:17:12 +0530

    Respect cross-region for object store

--- cmd/objectstore/objectstore_delete.go
+++ cmd/objectstore/objectstore_delete.go
@@ -24,10 +24,16 @@ var objectStoreDeleteCmd = &cobra.Command{
 	Example: "civo objectstore delete OBJECTSTORE_NAME",
 	Args:    cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
+		utility.EnsureCurrentRegion()
+
 		client, err := config.CivoAPIClient()
 		if err != nil {
 			utility.Error("Creating the connection to Civo's API failed with %s", err)
 			os.Exit(1)
+		}
+
+		if common.RegionSet != "" {
+			client.Region = common.RegionSet
 		}
 
 		if len(args) == 1 {
