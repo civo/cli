084a061fef80ec083731863ee7453617e6b4ef91
satakshigarg <satakshi@civo.com>
2022-12-06 14:21:07 +0530

    Improved error description
    Signed-off-by: satakshigarg <satakshi@civo.com>

--- common/common.go
+++ common/common.go
@@ -52,7 +52,16 @@ func IsGHError(err error) error {
 	ghErr, ok := err.(*github.ErrorResponse)
 	if ok {
 		if ghErr.Response.StatusCode >= 400 && ghErr.Response.StatusCode < 500 {
-			return errors.Wrap(err, `Failed to query the GitHub API for updates.`)
+			return errors.Wrap(err, `Failed to query the GitHub API for updates.
+			This is most likely due to GitHub rate-limiting on unauthenticated requests.
+			To have the civo-cli make authenticated requests please:
+			  1. Generate a token at https://github.com/settings/tokens
+			  2. Set the token by either adding it to your ~/.gitconfig
+				 setting the GITHUB_TOKEN environment variable.
+			Instructions for generating a token can be found at:
+			https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line/
+			We call the github releases API to look for new releases.
+			More information about that API can be found here: https://developer.github.com/v3/repos/releases/`)
 		}
 		if ghErr.Response.StatusCode == http.StatusUnauthorized {
 			return errors.Wrap(err, "Your Github token is invalid. Check the [github] section in ~/.gitconfig\n")
