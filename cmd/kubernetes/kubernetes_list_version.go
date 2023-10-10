b2f2b0b883e24458b84cd6aa190b71b71d7587f9
Alessandro Argentieri <alessandro@civo.com>
2023-10-10 09:44:06 +0200

    show deprecated versions (#345)
    Supported kubernetes version are listed even if they're deprecated. They cannot be used for new clusters but supported by the system.

--- cmd/kubernetes/kubernetes_list_version.go
+++ cmd/kubernetes/kubernetes_list_version.go
@@ -44,9 +44,6 @@ If you wish to use a custom format, the available fields are:
 
 		ow := utility.NewOutputWriter()
 		for _, version := range kubeVersions {
-			if version.Type == "deprecated" {
-				continue
-			}
 
 			ow.StartLine()
 
