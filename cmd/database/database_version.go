851c2b709069a9b10b70e53ce55adcecf8b30b2d
uzair <uzaxirr@gmail.com>
2024-02-24 03:12:06 +0530

    set region in client

--- cmd/database/database_version.go
+++ cmd/database/database_version.go
@@ -14,11 +14,16 @@ var dbVersionListCmd = &cobra.Command{
 	Example: `civo db versions`,
 	Short:   "List all the available database versions",
 	Run: func(cmd *cobra.Command, args []string) {
+		utility.EnsureCurrentRegion()
 
 		client, err := config.CivoAPIClient()
 		if err != nil {
 			utility.Error("Creating the connection to Civo's API failed with %s", err)
 			os.Exit(1)
+		}
+
+		if common.RegionSet != "" {
+			client.Region = common.RegionSet
 		}
 
 		dbVersions, err := client.ListDBVersions()
