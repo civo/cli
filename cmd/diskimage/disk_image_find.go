86ee72f425a84838645abc24db6797032afa17b9
Uzair Ali <72073401+uzaxirr@users.noreply.github.com>
2025-04-22 22:37:41 +0530

    refactor(diskimage): rename ImageSize to ImageSizeBytes and update output formatting

--- cmd/diskimage/disk_image_find.go
+++ cmd/diskimage/disk_image_find.go
@@ -1,6 +1,7 @@
 package diskimage
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/civo/cli/common"
@@ -28,7 +29,23 @@ var diskImageFindCmd = &cobra.Command{
 			os.Exit(1)
 		}
 
-		ow := utility.NewOutputWriterWithMap(map[string]string{"id": diskImg.ID, "name": diskImg.Name, "version": diskImg.Version, "state": diskImg.State, "distribution": diskImg.Distribution})
+		ow := utility.NewOutputWriter()
+
+		ow.AppendDataWithLabel("id", diskImg.ID, "ID")
+		ow.AppendDataWithLabel("name", diskImg.Name, "Name")
+		ow.AppendDataWithLabel("version", diskImg.Version, "Version")
+		ow.AppendDataWithLabel("state", diskImg.State, "State")
+		ow.AppendDataWithLabel("distribution", diskImg.Distribution, "Distribution")
+		ow.AppendDataWithLabel("initial_user", diskImg.InitialUser, "Initial User")
+		ow.AppendDataWithLabel("os", diskImg.OS, "OS")
+		ow.AppendDataWithLabel("description", diskImg.Description, "Description")
+		ow.AppendDataWithLabel("label", diskImg.Label, "Label")
+		ow.AppendDataWithLabel("disk_image_url", diskImg.DiskImageURL, "Disk Image URL")
+		ow.AppendDataWithLabel("disk_image_size_bytes", fmt.Sprintf("%d", diskImg.DiskImageSizeBytes), "Disk Image Size (bytes)")
+		ow.AppendDataWithLabel("logo_url", diskImg.LogoURL, "Logo URL")
+		ow.AppendDataWithLabel("created_at", diskImg.CreatedAt.String(), "Created At")
+		ow.AppendDataWithLabel("created_by", diskImg.CreatedBy, "Created By")
+		ow.AppendDataWithLabel("distribution_default", fmt.Sprintf("%t", diskImg.DistributionDefault), "Distribution Default")
 
 		switch common.OutputFormat {
 		case "json":
@@ -36,7 +53,7 @@ var diskImageFindCmd = &cobra.Command{
 		case "custom":
 			ow.WriteCustomOutput(common.OutputFields)
 		default:
-			ow.WriteTable()
+			ow.WriteKeyValues()
 		}
 	},
 }
