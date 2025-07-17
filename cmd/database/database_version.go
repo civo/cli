0c6f7069b894828f41e98f7ffa1f8bac2cb803d4
alessandroargentieri <alessandro@civo.com>
2025-07-17 15:52:05 +0200

    correctly visualise all the DB versions (not just the defaults)

--- cmd/database/database_version.go
+++ cmd/database/database_version.go
@@ -1,11 +1,13 @@
 package database
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/civo/cli/common"
 	"github.com/civo/cli/config"
 	"github.com/civo/cli/utility"
 	"github.com/spf13/cobra"
-	"os"
 )
 
 var dbVersionListCmd = &cobra.Command{
@@ -31,13 +33,18 @@ var dbVersionListCmd = &cobra.Command{
 			utility.Error("%s", err)
 			os.Exit(1)
 		}
+		fmt.Println(dbVersions)
 
 		ow := utility.NewOutputWriter()
 
+		// Iterate through each database type and all its versions
 		for dbName, versionDetails := range dbVersions {
-			ow.StartLine()
-			ow.AppendDataWithLabel("name", dbName, "Name")
-			ow.AppendDataWithLabel("version", versionDetails[0].SoftwareVersion, "version")
+			for _, version := range versionDetails {
+				ow.StartLine()
+				ow.AppendDataWithLabel("name", dbName, "Name")
+				ow.AppendDataWithLabel("version", version.SoftwareVersion, "Version")
+				ow.AppendDataWithLabel("default", fmt.Sprintf("%t", version.Default), "Default")
+			}
 		}
 
 		switch common.OutputFormat {
