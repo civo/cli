dd62518afc1ccced192694ebf756ffe448e08812
Alex Lubbock <code@alexlubbock.com>
2026-04-22 23:38:05 +0100

    fix(instance): add output format handling to public-ip and recovery commands

--- cmd/instance/instance_recovery.go
+++ cmd/instance/instance_recovery.go
@@ -47,8 +47,6 @@ Example:
 			utility.Error("%s", err)
 			os.Exit(1)
 		}
-		fmt.Println("Instance ID is: ", instance.ID)
-
 		if args[0] == "enable" {
 			_, err := client.EnableRecoveryMode(instance.ID)
 			if err != nil {
@@ -63,10 +61,23 @@ Example:
 			}
 		}
 
-		if args[0] == "enable" {
-			fmt.Printf("Recovery mode has been enabled for instance %s\n", utility.Green(instance.Hostname))
-		} else {
-			fmt.Printf("Recovery mode has been disabled for instance %s\n", utility.Green(instance.Hostname))
+		ow := utility.NewOutputWriter()
+		ow.StartLine()
+		ow.AppendDataWithLabel("id", instance.ID, "ID")
+		ow.AppendDataWithLabel("hostname", instance.Hostname, "Hostname")
+		ow.AppendDataWithLabel("recovery_mode", args[0], "Recovery Mode")
+
+		switch common.OutputFormat {
+		case "json":
+			ow.WriteSingleObjectJSON(common.PrettySet)
+		case "custom":
+			ow.WriteCustomOutput(common.OutputFields)
+		default:
+			if args[0] == "enable" {
+				fmt.Printf("Recovery mode has been enabled for instance %s\n", utility.Green(instance.Hostname))
+			} else {
+				fmt.Printf("Recovery mode has been disabled for instance %s\n", utility.Green(instance.Hostname))
+			}
 		}
 	},
 }
